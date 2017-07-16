2643791dc472d535c38549c8e9b9346fa81f9ab6
Lindsey Fischer <lindsfisch@gmail.com>
2017-07-16 10:45:19 -0500

    set up routes and api. updated html

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendArray = [
+    {
+        name: 'Lindsey',
+        photo: 'https://placekitten.com/200/300',
+        scores: ["5","1","4","4","5","1","2","5","4","1"]
+    }
+];
+
+module.exports = friendArray;
