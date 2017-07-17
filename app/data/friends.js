34d47aaf1cf0ef9f985e8f4efede895df349d4dc
Lindsey Fischer <lindsfisch@gmail.com>
2017-07-16 20:37:12 -0500

    added logic for friendmatch

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,34 @@
 var friendArray = [
     {
-        name: 'Lindsey',
-        photo: 'https://placekitten.com/200/300',
-        scores: ["5","1","4","4","5","1","2","5","4","1"]
-    }
+        name: 'Ron Burgundy',
+        photo: 'https://vignette4.wikia.nocookie.net/anchorman/images/1/10/Ron_burgundy.jpg/revision/latest?cb=20120329160125',
+        scores: ["5","5","1","4","4","3", "2", "1", "3", "2"]
+    },
+    {
+        name: 'Buddy',
+        photo: 'http://cdn3.meme.am/cache/images/folder979/600x600/14795979/happy-buddy-the-elf.jpg',
+        scores: ["2", "2", "4", "5", "2", "3", "4", "2", "1", "2"]
+    },
+        {
+        name: 'Brennan Huff',
+        photo: 'http://starsmedia.ign.com/stars/image/object/143/14353672/brennan-huff_pictureboxart_160w.jpg',
+        scores: ["1", "4", "5", "2", "5", "5", "1", "2", "3", "5"]
+    },
+        {
+        name: 'Chazz Reinhold',
+        photo: 'http://www.fusionmedstaff.com/wp-content/uploads/2015/01/Ma-the-meatloaf.jpg',
+        scores: ["1", "5", "4", "3", "2", "5", "1", "5", "3", "1"]
+    },
+        {
+        name: 'Allen Gamble',
+        photo: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-other-guys-will-ferrell.jpg',
+        scores: ["3", "1", "3", "2", "5", "2", "3", "4", "4", "3"]
+    },
+    {
+        name: 'Ricky Bobby',
+        photo: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-talladega-nights-the-ballad-of-ricky-bobby-will-ferrell.jpg',
+        scores: ["5", "3", "3", "2", "4", "3", "5", "4", "3", "2"]
+    },
 ];
 
 module.exports = friendArray;
